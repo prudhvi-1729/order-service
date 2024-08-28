d383d476bd65302eab5329dbea34fef570cc9be5
prudhvi kumar <prudhvibaddam@gmail.com>
2024-08-28 18:14:30 +0530

    added circuit breaker to inventory service along with request timeout for api call to inventory service from order service

--- src/main/java/com/capstone/order_service/config/RestClientConfig.java
+++ src/main/java/com/capstone/order_service/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.capstone.order_service.config;
 
 import com.capstone.order_service.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -23,6 +28,14 @@ public class RestClientConfig {
         var httpServerProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
 
         return httpServerProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings =  ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 
 }
